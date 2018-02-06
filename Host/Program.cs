496464a9e3f083de27dc698cd53600677c0cef2e
tnbuig <Oran.Gerbovski>
2018-02-06 23:11:40 +0100

    Add project files.

---
+++ Host/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(SimpleService.SimpleService)))
+            {
+                host.Open();
+                Console.WriteLine("Host Started @ " + DateTime.Now.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
